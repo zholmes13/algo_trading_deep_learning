d8f8cd50599f39c2f4ca9bf803f99aed9e4441bf
zholmes13 <56315629+zholmes13@users.noreply.github.com>
2022-02-15 14:06:02 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="AlgoTradingDeepLearning",
+    version="0.0.0",
+    author="Zach Holmes",
+    author_email="zholmes13@gmail.com",
+    description="AlgoTradingDeepLearning is a deep learning framework for algorithmic trading.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zholmes13/algo_trading_deep_learning",
+    license="MIT",
+    packages=find_packages()
+)
